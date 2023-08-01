85b04d9ff1e746383a6fa7018fff1f2f1aab7e8a
KeeganJones <keegs112@gmail.com>
2023-07-31 20:46:24 -0500

    Add BookCard component

---
+++ src/components/Collapse.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-sm mt-2" :class="{ 'md:text-lg': size === 'lg' }">
+    <button class="flex items-center w-full mb-1" @click="isOpen = !isOpen">
+      <slot name="title"></slot>
+      <i
+        class="fa-solid fa-chevron-down transition-transform ml-auto"
+        :class="{ 'fa-rotate-180': isOpen }"
+      ></i>
+    </button>
+    <div v-if="isOpen" class="text-gray-400">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const props = defineProps({ size: String });
+const isOpen = ref(false);
+</script>
