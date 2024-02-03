67a93ce35b89f91b1d2cfa1c7a284423f2a54157
KeeganJones <keegs112@gmail.com>
2024-02-02 21:57:49 -0600

    Install pinia

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import './style.css';
 
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
 import App from './App.vue';
 import { setProfile, supabase, userSession } from './lib/supabase';
 
-createApp(App).mount('#app');
+const pinia = createPinia();
+
+createApp(App).use(pinia).mount('#app');
 
 supabase.auth.onAuthStateChange(async (event, session) => {
     userSession.value = session;
